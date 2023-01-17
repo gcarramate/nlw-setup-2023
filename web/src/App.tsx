f4a177779986b9244f0358320f34993d947d5fdd
Gabriel Carramate <glcarramate@hotmail.com>
2023-01-17 15:19:13 -0300

    finish web initial config

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,14 @@
+import './styles/global.css'
+
 import { Habit } from "./components/Habit";
 
 function App() {
   return (
-    <Habit />
+    <>
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit />
+    </>
   )
 }
 
