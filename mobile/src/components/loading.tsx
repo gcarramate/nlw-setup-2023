233054032ba7670f0eeb3af2f9ef11a84a7aac09
Gabriel Carramate <glcarramate@hotmail.com>
2023-01-19 12:51:55 -0300

    finish tailwind config, add svg support, fix

--- mobile/src/components/loading.tsx
+++ mobile/src/components/loading.tsx
@@ -1,6 +1,6 @@
 import { ActivityIndicator, View } from "react-native";
 
-export function Loading(){
+export function Loading() {
   return(
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <ActivityIndicator color="#7C3AED" />
