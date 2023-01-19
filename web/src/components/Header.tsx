d3f8d0d02824a2d9d1d0cc92401dc7c1a7bc7ec2
Gabriel Carramate <glcarramate@hotmail.com>
2023-01-19 01:51:35 -0300

    create summary table component and add to app

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -2,7 +2,7 @@ import logoImage from '../assets/logo.svg';
 import { Plus } from "phosphor-react";
 
 export function Header() {
-  return(
+  return (
     <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
       <img src={logoImage} alt='Habits' />
 
