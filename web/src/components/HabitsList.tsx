0a64b035a9a09a766fedfd1e2e04d65c645d0042
Gabriel Carramate <glcarramate@hotmail.com>
2023-01-22 20:32:33 -0300

    integrate habit toggle

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -1,4 +1,5 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
+import dayjs from "dayjs";
 import { Check } from "phosphor-react";
 import { useEffect, useState } from "react";
 import { api } from "../lib/axios";
@@ -28,27 +29,51 @@ export function HabitsList({ date }: HabitsListProps) {
       setHabitsInfo(response.data);
     })
   }, [])
-return (
-  <div className="mt-6 flex flex-col gap-3">
-    {habitsInfo?.possibleHabits.map(habit => {
-      return(
-        <Checkbox.Root
-        className="flex items-center gap-3 group"
-        checked={habitsInfo.completedHabits.includes(habit.id)}
-        key={habit.id}
-        >
-          <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-            <Checkbox.Indicator>
-              <Check size={20} className="text-white" />
-            </Checkbox.Indicator>
-          </div>
-    
-          <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-            {habit.title}
-          </span>
-        </Checkbox.Root>
-      )
-    })}
-  </div>
+
+  const isDateInPast = dayjs(date)
+  .endOf('day')
+  .isBefore(new Date())
+
+  async function handleToggleHabit(habitId: string) {
+    api.patch(`/habits/${habitId}/toggle`)
+
+    let completedHabits: string[];
+    const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId);
+    if (isHabitAlreadyCompleted) {
+      completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId);
+      
+    } else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId]
+    }
+    setHabitsInfo({
+      possibleHabits: habitsInfo!.possibleHabits,
+      completedHabits
+    })
+  }
+  
+  return (
+    <div className="mt-6 flex flex-col gap-3">
+      {habitsInfo?.possibleHabits.map(habit => {
+        return(
+          <Checkbox.Root
+          className="flex items-center gap-3 group"
+          onCheckedChange={() => handleToggleHabit(habit.id)}
+          checked={habitsInfo.completedHabits.includes(habit.id)}
+          disabled={isDateInPast}
+          key={habit.id}
+          >
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+              <Checkbox.Indicator>
+                <Check size={20} className="text-white" />
+              </Checkbox.Indicator>
+            </div>
+      
+            <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+              {habit.title}
+            </span>
+          </Checkbox.Root>
+        )
+      })}
+    </div>
   )
 }
