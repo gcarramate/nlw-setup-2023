ea0a74552f2643eac76773b5c9596a36ae0137cc
Gabriel Carramate <glcarramate@hotmail.com>
2023-01-22 20:44:39 -0300

    synchronize complete habits

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -3,18 +3,24 @@ import clsx from 'clsx';
 import { ProgressBar } from "./ProgressBar";
 import dayjs from "dayjs";
 import { HabitsList } from "./HabitsList";
+import { useState } from "react";
 
 interface HabitDayProps {
   date: Date
   amount?: number
-  completed?: number
+  defaultCompleted?: number
 }
 
-export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
+export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayProps) {
+  const [completed, setCompleted] = useState(defaultCompleted);
   const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0;
 
   const dayAndMonth = dayjs(date).format('DD/MM');
-  const dayOfWeek = dayjs(date).format('dddd')
+  const dayOfWeek = dayjs(date).format('dddd');
+
+  function handleCompletedChanged(completed: number) {
+    setCompleted(completed);
+  }
 
   return (
     <Popover.Root>
@@ -33,7 +39,7 @@ export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
           <span className="mt-1 font-extrabold leading-tight text-3xl">{dayAndMonth}</span>
 
           <ProgressBar progress={completedPercentage} />
-          <HabitsList date={date} />
+          <HabitsList date={date} onCompletedChanged={handleCompletedChanged} />
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
       </Popover.Portal>
