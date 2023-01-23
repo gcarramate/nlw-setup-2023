f23b23e0cdb16868991921243e52e79add4cb672
Gabriel Carramate <glcarramate@hotmail.com>
2023-01-22 21:11:19 -0300

    add new changes

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -32,7 +32,7 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayPro
         'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
         'bg-violet-500 border-violet-400': completedPercentage >= 80,
       })} />
-   
+    
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
           <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
